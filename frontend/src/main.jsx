838b3f58d1e845447c807273a26d46ccf11e91d9
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2025-01-10 23:26:18 +0530

    added no change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { createContext, StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App.jsx";
 
 
