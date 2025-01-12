bb8bc07c4b67eb41c88f733a938511ac7560e217
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2025-01-12 18:18:12 +0530

    added context

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useContext, useState } from "react"
+import con
 
 function Navbar() {
-    
+    const [show,setShow] = useState();
+    const { isAuthenticated, setIsAuthenticated } = useContext(Context)
   return (
     <div>Navbar</div>
   )
