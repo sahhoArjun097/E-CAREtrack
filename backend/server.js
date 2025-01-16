9f2014c6cfd74213d5d429273c035a1fb4a83fc9
Nishant saini <ns0109375@gmail.com>
2025-01-16 22:06:04 +0530

    making dotenv config file

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
 import app from "./app.js";
-import cloudinary from "cloudinary"
+import cloudinary from "cloudinary";
+import config from "./config/index.js";
 cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
-    api_key:process.env.CLOUDINARY_API_KEY
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening at port ${process.env.PORT}`);
+  cloud_name: config.CLOUDINARY_CLOUD_NAME,
+  api_secret: config.CLOUDINARY_API_SECRET,
+  api_key: config.CLOUDINARY_API_KEY,
 });
 
-
-
-
-
+app.listen(config.PORT, () => {
+  console.log(`Server listening at port ${config.PORT}`);
+});
